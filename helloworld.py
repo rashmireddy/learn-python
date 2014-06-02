722a558b0fbc5925f108fae839529b010c3b0224
rashmireddy <irashmi.reddy@gmail.com>
2014-06-01 22:56:49 -0700

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+
+print "Hello World!..."
+
+print "scalar testing"
+scalar=10
+print scalar
+
+scalar="rashmi"
+print scalar
+
+scalar=10.10
+print scalar
+
+print "testing list"
+list=[10,20,30,40]
+print list
+
+list=["as","asdf","qwe","qwert"]
+print list
+
+list=[10,"sdfsdf",20]
+print list
+
+print "tesing hash/dict"
+dict={"key1":"value1","key2":10,"key3":[10,11,12],"key4":{"k":"v"}}
+print dict
+print dict["key4"]["k"]
+print list[1]
